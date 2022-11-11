c1b2568f5e7c84aaa12019831d0703468ec67538
ZhiKiatGan <zhikiatgan@hotmail.com>
2022-11-11 21:26:40 +0800

    add auto redirect from root to swagger ui

--- ordering/src/main/java/com/ifood/ordering/OrderingApplication.java
+++ ordering/src/main/java/com/ifood/ordering/OrderingApplication.java
@@ -10,13 +10,22 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EnableSwagger2
+@RestController
 public class OrderingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderingApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public ModelAndView  rootRedirect() {
+		return new ModelAndView("redirect:/swagger-ui.html" );
 	}
 	
 	@Bean
